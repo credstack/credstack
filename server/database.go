9f737f7575929c3fc9ad58604a423cee928a11c2
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-11 14:02:17 -0400

    Implemented a helper method for setting mongo authentication

--- server/database.go
+++ server/database.go
@@ -52,6 +52,27 @@ func (database *Database) Database() *mongo.Database {
 }
 
 /*
+SetSCRAMAuthentication - Instructs the mongo.Client to use SCRAM authentication when establishing
+a connection to the MongoDB database. SCRAM-SHA-256 is used as the default authentication mechanism
+here as it balances security with performance while establishing connections.
+
+The default authentication source is set to the value passed in Database.defaultDatabase. Generally
+using 'admin' as the default authentication source is not recommended as users should be isolated
+only to the database that they need access to. This client expects read/write permissions to the default
+database collections that are created with the Database.Init method.
+*/
+func (database *Database) SetSCRAMAuthentication(username string, password string) {
+	credential := options.Credential{
+		AuthMechanism: "SCRAM-SHA-256",
+		AuthSource:    database.defaultDatabase,
+		Username:      username,
+		Password:      password,
+	}
+
+	database.options.SetAuth(credential)
+}
+
+/*
 Connect - General wrapper around mongo.Connect. Generally, the mongo session created with
 this function should be re-used across multiple calls to ensure that excess resources
 are not wasted initiating additional connections to MongoDB.
