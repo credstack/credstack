f11c65b9bacdf7147a10b9ea65dbb85639c49397
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-14 07:29:54 -0400

    Defined a basic structure for LogOptions. This is going to contain some configurable values for the logger

--- options/log.go
+++ options/log.go
@@ -1,3 +1,29 @@
 package options
 
+/*
+LogOptions - A container for any configurable options for the Zap Logger. Not
+a lot of options are exposed here as the logger isn't designed to be very
+featureful.
+*/
+type LogOptions struct {
+	// UseFileLogging - If set to true, log files will be written in JSON format
+	UseFileLogging bool
 
+	// LogPath - The directory that logs should be saved under
+	LogPath string
+
+	// LogLevel - A string determining how verbose logs should be. Can be: Info (default), Debug, All
+	LogLevel string
+}
+
+/*
+Log - Returns a LogOptions structure with some sensible defaults. File logging is disabled by default
+to avoid adding un-needed complexity.
+*/
+func Log() *LogOptions {
+	return &LogOptions{
+		UseFileLogging: false,
+		LogPath:        "/.credstack/logs",
+		LogLevel:       "", // change this
+	}
+}
