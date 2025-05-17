544b6bedad4b2fad18eee8c0b69d2ccd3d620d48
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-17 13:20:35 -0400

    Defined a basic abstraction for the Log along with a constructor

--- server/log.go
+++ server/log.go
@@ -1,3 +1,26 @@
 package server
 
+import "github.com/stevezaluk/credstack-lib/options"
 
+/*
+Log - An abstraction for the Logger. Handles any logic for creating and writing log files here
+*/
+type Log struct {
+	// options -
+	options *options.LogOptions
+}
+
+/*
+NewLog - Constructs a new Log using the values passed in its parameters. If an options structure is not passed in this
+functions parameter, then the Log is initialized with default values. Additionally, if more than 1 are passed here,
+only the first is used.
+*/
+func NewLog(opts ...*options.LogOptions) *Log {
+	if len(opts) == 0 {
+		opts = append(opts, new(options.LogOptions))
+	}
+
+	return &Log{
+		options: opts[0],
+	}
+}
