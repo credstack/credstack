19ecb0b81f95d0359e4e3a35f14ac08529cc779c
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-18 19:34:36 -0400

    Implemented support for hashing secrets using argon2

--- user/credential.go
+++ user/credential.go
@@ -1,3 +1,53 @@
 package user
 
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/stevezaluk/credstack-lib/options"
+	"golang.org/x/crypto/argon2"
+	"io"
+)
 
+/*
+hashSecret - Generates a ArgonV2ID hash for the secret provided in the first parameter. Any options that are provided
+here for hashing should be persisted using the user.UserCredential model as this ensures the same ones can be used
+when you need to validate the hash
+
+Unlike other functions implemented in this library, the opts parameter is forced. This is done to ensure that the
+caller is fully aware of the parameters that they are passing to this function.
+*/
+func hashSecret(secret []byte, opts *options.CredentialOptions) (string, string, error) {
+	/*
+		First we generate some random bytes to serve as our salt for our password hash
+	*/
+	salt := make([]byte, opts.SaltLength)
+
+	/*
+		Then we inject randomness into the bytes to increase entropy
+	*/
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		return "", "", err
+	}
+
+	/*
+		Then we generate our ArgonV2ID hash so that we can persist it for the user
+	*/
+	key := argon2.IDKey(
+		secret,
+		salt,
+		opts.Time,
+		opts.Memory,
+		opts.Threads,
+		opts.KeyLength,
+	)
+
+	/*
+		Finally we want to store our hash and salt as a base64 encoded string, as when we inject randomness,
+		non-printable characters can be included. This provides a safer way of persisting our salts
+	*/
+	encodedHash := base64.URLEncoding.EncodeToString(key)
+	encodedSalt := base64.URLEncoding.EncodeToString(salt)
+
+	return encodedHash, encodedSalt, nil
+}
