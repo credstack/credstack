ec6631c578f51b1f93b46b16a8de5c6538342cae
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-19 08:40:48 -0400

    Implemented support for generating standard unix timestamps and human-readable string timestamps

--- utils/time.go
+++ utils/time.go
@@ -1,3 +1,17 @@
 package utils
 
+import "time"
 
+/*
+UnixTimestamp - Returns a UNIX timestamp representing the current date/time
+*/
+func UnixTimestamp() int64 {
+	return time.Now().Unix()
+}
+
+/*
+StringTimestamp - Returns a string representing a human-readable timestamp. Most commonly used in log file names
+*/
+func StringTimestamp() string {
+	return time.Now().Format("20060102T150405")
+}
