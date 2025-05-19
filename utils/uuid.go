2a85d88d516c97107d4da1af2e08a421301ae46b
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-19 08:36:42 -0400

    Implemented a small function for generating UUID's from a basis

--- utils/uuid.go
+++ utils/uuid.go
@@ -1,3 +1,15 @@
 package utils
 
+import uuid2 "github.com/google/uuid"
 
+/*
+GenerateUUID - Generates a basic version 5 UUID to use in the header.Identifier field. The basis that is passed
+in the parameter here is hashed along with the UUID namespace URL and a new UUID is generated from it. Using a
+basis for this generation provides an additional layer of protection against duplication as if the same basis
+is used, then the same UUID is generated
+*/
+func GenerateUUID(basis string) string {
+	uuid := uuid2.NewSHA1(uuid2.NameSpaceURL, []byte(basis))
+
+	return uuid.String()
+}
