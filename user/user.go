48fab60f4eca866daaedaf075b1b5da31b479e0b
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-20 18:06:12 -0400

    Updated the NewUser function with better comments and removed functionality for generating headers. This is going to be done at RegisterUser to discourage callers from using this as a replacement for this

--- user/user.go
+++ user/user.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/stevezaluk/credstack-lib/header"
 	"github.com/stevezaluk/credstack-lib/server"
 	"github.com/stevezaluk/credstack-models/proto/user"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -17,21 +16,15 @@ var ErrUserAlreadyExists = errors.New("user: User already exists")
 var ErrUserMissingIdentifier = errors.New("user: User is either missing an identifier or an email address")
 
 /*
-NewUser - Provides a simple wrapper around a MongoDB query to insert a new user into the database. This performs no
-credential generation or validation so you should really use RegisterUser for this. Any header.Identifier that was
-passed here will be overwritten, and generated from the email address of the user. A user email address is immutable
-so it provides a good basis for identifier.
+NewUser - Provides a simple wrapper around collection.InsertOne to insert newly created users into the database. No
+validation or header generation is provided here so you should really use RegisterUser here if you want to create
+a new user. Only use this function if you have a pre-generated user struct that you want to implement
 
-Any errors that occur here will be returned in the form of named errors. A nil return type indicates that the
-call was successful
+This function, similar to others in service packages expect that you have unique indexes built around collections
+as this won't make an additional database call to validate that the user exists. This only checks for a write exception
+upon making an InsertOne call. See the doc's for more information on where/how indexes should be created.
 */
 func NewUser(ctx *server.Server, user *user.User) error {
-	if user.Email == "" {
-		return ErrUserMissingIdentifier
-	}
-
-	user.Header = header.NewHeader(user.Email)
-
 	_, err := ctx.Database().Collection("user").InsertOne(context.Background(), user)
 	if err != nil {
 		/*
