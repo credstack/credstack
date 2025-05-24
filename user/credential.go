de0232e1faea50a4f2e9e63c0dd4752b42505659
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-23 22:07:27 -0400

    Implemented support for validating user password hashes

--- user/credential.go
+++ user/credential.go
@@ -2,12 +2,17 @@ package user
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"github.com/stevezaluk/credstack-lib/options"
 	"github.com/stevezaluk/credstack-models/proto/user"
 	"golang.org/x/crypto/argon2"
 	"io"
 )
+
+// ErrUserCredentialInvalid - Provides a named error for when user credential validation fails
+var ErrUserCredentialInvalid = errors.New("user: invalid credentials")
 
 /*
 hashSecret - Generates a ArgonV2ID hash for the secret provided in the first parameter. Any options that are provided
@@ -72,4 +77,53 @@ func NewCredential(secret string, opts *options.CredentialOptions) (*user.UserCr
 		KeyLength:  opts.KeyLength,
 		SaltLength: opts.SaltLength,
 	}, nil
+}
+
+/*
+ValidateSecret - Validates that a user provided secret is identical to the secret provided in the credential parameter
+*/
+func ValidateSecret(secret []byte, credential *user.UserCredential) error {
+	/*
+		To start the validation process we first need to base64 decode the salt that was
+		stored in MongoDB. We use make to allocate us a byte array of the requested salt length
+	*/
+	decodedSalt := make([]byte, credential.SaltLength)
+	n, err := base64.URLEncoding.Decode(decodedSalt, []byte(credential.Salt))
+	if err != nil {
+		return err
+	}
+
+	/*
+		We always want to decode our values first to ensure that we can catch any basic errors
+		before we start Argon hash generation
+	*/
+	decodedHash := make([]byte, credential.KeyLength)
+	m, err := base64.URLEncoding.Decode(decodedHash, []byte(credential.Key))
+	if err != nil {
+		return err
+	}
+
+	/*
+		The once we have our decoded salt, we can use Argon to generate us a fresh hash
+		using the parameters stored with the user credential
+	*/
+	key := argon2.IDKey(
+		secret,
+		decodedSalt[:n],
+		credential.Time,
+		credential.Memory,
+		uint8(credential.Threads),
+		credential.KeyLength,
+	)
+
+	/*
+		subtle.ConstantTimeCompare provides a time-safe way of comparing password hashes. This protects
+		us against time-based attacks during comparison
+	*/
+	result := subtle.ConstantTimeCompare(key, decodedHash[:m])
+	if result != 1 {
+		return ErrUserCredentialInvalid
+	}
+
+	return nil
 }
