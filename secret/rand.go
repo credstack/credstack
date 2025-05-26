effa019d0ed5269e7321829bacde3a6d37a9e743
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-26 12:50:43 -0400

    Implemented support for generating random byte arrays

--- secret/rand.go
+++ secret/rand.go
@@ -1,3 +1,19 @@
 package secret
 
+import "crypto/rand"
 
+/*
+RandBytes - A function for generating cryptographically secure, random byte arrays of a fixed
+size. Most commonly used for generating Argon2 hashes, or secured version 5 UUIDs. The error returned
+from this function can be safely ignored as it is passed directly from rand.Read
+*/
+func RandBytes(length uint32) ([]byte, error) {
+	ret := make([]byte, length)
+
+	n, err := rand.Read(ret)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret[:n], nil
+}
