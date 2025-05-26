bb55410a926fd661508f1748ba3ff53e357b6201
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-26 19:04:52 -0400

    Implemented support for matching regular expressions

--- internal/regex.go
+++ internal/regex.go
@@ -1,3 +1,15 @@
 package internal
 
+import "regexp"
 
+/*
+MatchRegex - Simple function for matching regular expressions. The parameter 're' is a pointer to a Regexp struct
+as we don't want to re-compile these on every call to MatchRegex
+*/
+func MatchRegex(target string, re *regexp.Regexp) bool {
+	if re.MatchString(target) {
+		return true
+	}
+
+	return false
+}
