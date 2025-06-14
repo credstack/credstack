5deb10a2b0ce186794ec46bcff7d85823a6f2d89
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-14 18:07:25 -0400

    Implemented a basic error

--- key/jwk.go
+++ key/jwk.go
@@ -1,3 +1,7 @@
 package key
 
+import (
+	credstackError "github.com/stevezaluk/credstack-lib/errors"
+)
 
+var ErrGenerateKey = credstackError.NewError(500, "ERR_GENERATING_KEY", "jwk: Failed to generate cryptographic key")
