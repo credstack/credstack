abe78ef28c4c6c7cf815576951b8d9b0856c5f8c
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-14 18:06:51 -0400

    Updated GenerateKey to return a token.KeyPair value instead of directly returning the rsa.PrivateKey

--- key/rsa.go
+++ key/rsa.go
@@ -3,6 +3,8 @@ package key
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"github.com/stevezaluk/credstack-lib/proto/token"
+	"github.com/stevezaluk/credstack-lib/secret"
 )
 
 /*
@@ -10,7 +12,7 @@ GenerateKey - Generates a 4096-bit RSA Key Pair. The size on this is not adjusta
 with 4096-bit keys. After the key is generated, it is validated to ensure that it can be used for signing tokens. Any
 errors here are propagated with the second return type
 */
-func GenerateKey() (*rsa.PrivateKey, error) {
+func GenerateKey() (*token.KeyPair, error) {
 	/*
 		First we want to generate our key here. Since we don't need to conform to user provided size, we can always
 		use the 4096 as the size in bits.
@@ -27,5 +29,25 @@ func GenerateKey() (*rsa.PrivateKey, error) {
 		return nil, err
 	}
 
-	return key, nil
+	/*
+		Once we have our key generated, we want to marshal it to our model so that we can store it within Mongo
+		safely. We are encoding any big.Int values here as protobuf does not have a way of natively storing these
+		without losing precision. By converting them to bytes first, and then encoding them with base64, we can
+		guarantee that we won't lose any precision
+	*/
+	ret := &token.KeyPair{
+		Kty: "RSA",
+		Alg: "RS256",
+		Use: "sig",
+		N:   secret.EncodeBigInt(key.N),
+		E:   int64(key.E),
+		D:   secret.EncodeBigInt(key.D),
+		P:   secret.EncodeBigInt(key.Primes[0]),
+		Q:   secret.EncodeBigInt(key.Primes[1]),
+		Dp:  secret.EncodeBigInt(key.Precomputed.Dp),
+		Dq:  secret.EncodeBigInt(key.Precomputed.Dq),
+		Qi:  secret.EncodeBigInt(key.Precomputed.Qinv),
+	}
+
+	return ret, nil
 }
