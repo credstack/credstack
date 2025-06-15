4aa183c08447f9993d34d7cce2a92ce3cb0b7c10
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-15 16:31:29 -0400

    Removed RotateKeys for now, as I need a better way of rotating these after we sort out other issues with this implementation

--- key/jwk.go
+++ key/jwk.go
@@ -24,7 +24,7 @@ var ErrKeyIsNotValid = credstackError.NewError(500, "ERR_KEY_NOT_VALID", "jwk: T
 GetJWKS - Fetches all JSON Web Keys stored in the database and returns them as a slice. The length of this slice should
 really never exceed 2, as key.RotateKeys will remove old keys
 
-TODO: Remove JWKS model
+TODO: Update this to use JWKS model
 */
 func GetJWKS(serv *server.Server) ([]*key.JSONWebKey, error) {
 	var jwks []*key.JSONWebKey
@@ -178,78 +178,4 @@ func GetPublicKey(serv *server.Server, kid string) (*rsa.PublicKey, error) {
 	}
 
 	return &publicKey, nil
-}
-
-/*
-RotateJWKS - Rotates the JSON Web Key Set that tokens are being signed with. Any RS256 tokens that were issued previously
-with these key's will become invalid after this function call as there previously generated keys can not be used to validate
-them.
-
-This function is **very** slow, so executing it should be treated with care. On each call to this function 2 new RSA key's
-are generated and then converted into properly formatted JWK's. We want to store these in separate collections so that
-we don't need to convert private keys to public keys on each call to .wellknown/jwks.json. This winds up dragging
-execution time on this function as we now need to clear our both collections after key generation, and then re-insert
-them. 4 total database calls are consumed in this function.
-
-Thankfully, the poor performance on this function should not be incredibly impactful, as this only gets called in two
-scenarios: When credstack starts for the first time, and when the user requests a key rotation
-
-TODO: The implementation for this is broken. Start with only one key and then during rotation, add an additional one
-TODO: This does not support HS-256
-*/
-func RotateJWKS(serv *server.Server) error {
-	/*
-		We are using traditional arrays here over slices as we can always guarantee that the same number of keys are going
-		to be generated with this function. This helps us reduce some memory allocations, as every call to append forces
-		us to resize the array to accompany for the additional element
-	*/
-	privateKeys := make([]*key.PrivateJSONWebKey, 2)
-	jwks := make([]*key.JSONWebKey, 2)
-
-	/*
-		Ideally, this could be exposed to the user to allow them to select how many keys they want to include in a JWKS.
-		However, we hard code this value here as increasing the amount of keys in the JWKS slows down key selection
-		during token generation
-	*/
-	for i := 0; i < 2; i++ {
-		privateKey, jwk, err := GenerateKey()
-		if err != nil {
-			return fmt.Errorf("%w (%v)", ErrGenerateKey, err)
-		}
-
-		/*
-			To account for storing each of them in different collections, we need to different slices here for this as
-			well.
-		*/
-		privateKeys[i] = privateKey
-		jwks[i] = jwk
-	}
-
-	/*
-		To ensure that old keys don't get retained, we want to completely clear out both collections here. We want to
-		do this **after** key generation in the event that key generation fails for whatever reason. If we clear out the
-		collections before key generation and generation fails, then we are left fucked without any new keys and our
-		old ones gone
-	*/
-	for _, collection := range []string{"key", "jwk"} {
-		_, err := serv.Database().Collection(collection).DeleteMany(context.Background(), bson.D{})
-		if err != nil {
-			return fmt.Errorf("%w (%v)", server.ErrInternalDatabase, err)
-		}
-	}
-
-	/*
-		After we have our collections cleared out, we can move forward and insert our keys accordingly
-	*/
-	_, err := serv.Database().Collection("key").InsertMany(context.Background(), privateKeys)
-	if err != nil {
-		return fmt.Errorf("%w (%v)", server.ErrInternalDatabase, err)
-	}
-
-	_, err = serv.Database().Collection("jwk").InsertMany(context.Background(), jwks)
-	if err != nil {
-		return fmt.Errorf("%w (%v)", server.ErrInternalDatabase, err)
-	}
-
-	return nil
 }
