329d8bd8457f17d113677568e3c30f5b9a3acd16
Steven A. Zaluk <stevezaluk@gmail.com>
2025-07-04 15:52:31 -0400

    Updated valdiate.go to validate grant types in addition to audiences

--- application/validate.go
+++ application/validate.go
@@ -19,3 +19,21 @@ func ValidateAudience(app *applicationModel.Application, audience string) bool {
 
 	return false
 }
+
+/*
+ValidateGrantType - Validates that an application is allowed to issue tokens for a specific grant type. Returns true if it
+is allowed, returns false otherwise. If a nil application is provided in the first argument, then false is also returned
+*/
+func ValidateGrantType(app *applicationModel.Application, grantType applicationModel.GrantTypes) bool {
+	if app == nil {
+		return false
+	}
+
+	for _, grant := range app.GrantType {
+		if grant == grantType {
+			return true
+		}
+	}
+
+	return false
+}
