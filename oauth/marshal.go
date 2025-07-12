98e103e3da11948c8972ee22650c60f40ad0d47e
Steven A. Zaluk <stevezaluk@gmail.com>
2025-07-12 12:29:45 -0400

    Updated MarshalTokenResponse to properly reneder expiration date. This unfortunately does not support custom expirations for now

--- oauth/marshal.go
+++ oauth/marshal.go
@@ -1,7 +1,6 @@
 package oauth
 
 import (
-	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	credstackError "github.com/stevezaluk/credstack-lib/errors"
 	"github.com/stevezaluk/credstack-lib/proto/response"
@@ -19,13 +18,6 @@ TODO: Expires in is not rendering properly, showing expiration instead of token 
 TODO: This function feels kind of clunky...
 */
 func MarshalTokenResponse(tok *jwt.Token, signedString string) (*response.TokenResponse, error) {
-	expirationDate, err := tok.Claims.GetExpirationTime()
-	if err != nil {
-		// wrapping this error with ErrFailedToSignToken is not ideal as it can lead to some confusion on
-		// how this function failed but... oh well!
-		return nil, fmt.Errorf("%w (%v)", ErrMarshalTokenResponse, err)
-	}
-
 	/*
 		After we actually sign our token, we can quickly convert it back into a response.TokenResponse structure so that
 		it can be returned from the API
@@ -33,7 +25,7 @@ func MarshalTokenResponse(tok *jwt.Token, signedString string) (*response.TokenR
 	signedResponse := &response.TokenResponse{
 		AccessToken:  signedString,
 		TokenType:    "Bearer",
-		ExpiresIn:    uint32(expirationDate.Time.Unix()), // this is bad, could lose precision by down converting to uint32
+		ExpiresIn:    86400, // this is bad, not creating future support for custom expiration's
 		RefreshToken: "",
 		IdToken:      "",
 		Scope:        "",
