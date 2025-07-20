705e6a6734b6940c950fbe922c2dd60287027314
Steven A. Zaluk <stevezaluk@gmail.com>
2025-07-19 21:00:08 -0400

    Externalized token generation code to GenerateToken and moved it away from NewToken

--- oauth/token/token.go
+++ oauth/token/token.go
@@ -12,8 +12,6 @@ import (
 	"github.com/credstack/credstack-lib/server"
 	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/v2/mongo"
-	pbTimestamp "google.golang.org/protobuf/types/known/timestamppb"
-	"time"
 )
 
 // ErrFailedToSignToken - An error that gets wrapped when jwt.Token.SignedString returns an error
@@ -23,11 +21,12 @@ var ErrFailedToSignToken = credstackError.NewError(500, "ERR_FAILED_TO_SIGN", "t
 var ErrTokenCollision = credstackError.NewError(500, "ERR_TOKEN_COLLISION", "token: A duplicate access token was issued")
 
 /*
-NewToken - Provides a centralized area for token generation to occur. newToken provides the logic required for associating
-a token type it's associating handler. If a valid signing algorithm is used, then it will return its formatted token
-response, otherwise it will return ErrFailedToSignToken
+GenerateToken - Generates a token based on the Application and API that are passed in the parameter. Claims that are passed
+will be inserted into the generated token. Calling this function alone, does not store the tokens in the database and only
+generates the token. An instantiated server structure needs to be passed here to ensure that we can fetch the current
+active encryption key for token signing (RS256)
 */
-func NewToken(serv *server.Server, api *apiModel.API, app *applicationModel.Application, claims jwt.RegisteredClaims) (*tokenModel.TokenResponse, error) {
+func GenerateToken(serv *server.Server, api *apiModel.API, app *applicationModel.Application, claims jwt.RegisteredClaims) (*tokenModel.TokenResponse, error) {
 	var tokenResp *tokenModel.TokenResponse
 
 	switch api.TokenType.String() {
@@ -56,24 +55,15 @@ func NewToken(serv *server.Server, api *apiModel.API, app *applicationModel.Appl
 		return nil, fmt.Errorf("%w (%v)", ErrFailedToSignToken, "Invalid Signing Algorithm")
 	}
 
-	/*
-		We use a separate model here to track tokens internally as we need some additional metadata to properly
-		support revocation and introspection
+	return tokenResp, nil
+}
 
-		This isn't working with TTL indexes properly, so for now tokens will pretty much never get removed
-		from the database. This is a major limitation here, but without creating some hacky shit it will
-		have to exist.
-	*/
-	internalToken := &tokenModel.Token{
-		AccessToken:  tokenResp.AccessToken,
-		RefreshToken: tokenResp.RefreshToken,
-		IdToken:      tokenResp.IdToken,
-		ClientId:     app.ClientId,
-		ExpiresIn:    tokenResp.ExpiresIn,
-		ExpiresAt:    pbTimestamp.New(time.Now().Add(time.Duration(tokenResp.ExpiresIn))),
-		Scope:        tokenResp.Scope,
-	}
-
+/*
+NewToken - Provides a centralized area for token generation to occur. newToken provides the logic required for associating
+a token type it's associating handler. If a valid signing algorithm is used, then it will return its formatted token
+response, otherwise it will return ErrFailedToSignToken
+*/
+func NewToken(serv *server.Server, token *tokenModel.Token) error {
 	/*
 		Were currently just storing the plain old token response here, which may pose some issues down the road specifically
 		if we want to implement functionality for revoking tokens for a specific user. This will fit the token revocation
@@ -83,18 +73,18 @@ func NewToken(serv *server.Server, api *apiModel.API, app *applicationModel.Appl
 		Keep in mind, JWTs are stateless. So "revoking" a token, really just means that the token will not be reported
 		as active by the token introspection endpoint
 	*/
-	_, err := serv.Database().Collection("token").InsertOne(context.Background(), internalToken)
+	_, err := serv.Database().Collection("token").InsertOne(context.Background(), token)
 	if err != nil {
 		var writeError mongo.WriteException
 		if errors.As(err, &writeError) {
 			if writeError.HasErrorCode(11000) { // 11000 is the error code for a WriteError. This should be a const
-				return nil, ErrTokenCollision // this should almost never occur, but we check for it regardless
+				return ErrTokenCollision // this should almost never occur, but we check for it regardless
 			}
 		}
 
 		// always return a wrapped internal database error here
-		return nil, fmt.Errorf("%w (%v)", server.ErrInternalDatabase, err)
+		return fmt.Errorf("%w (%v)", server.ErrInternalDatabase, err)
 	}
 
-	return tokenResp, nil
+	return nil
 }
