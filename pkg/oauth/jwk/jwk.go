700b04aea9170e971ff559295f37022cb37b4875
Steven A. Zaluk <stevezaluk@gmail.com>
2025-08-16 17:34:15 -0400

    refactor: Implemented a go-native model for JWKS and updated types that the GetJWKS function uses

--- pkg/oauth/jwk/jwk.go
+++ pkg/oauth/jwk/jwk.go
@@ -41,14 +41,22 @@ type JSONWebKey struct {
 }
 
 /*
+JSONWebKeySet - Represents a list of public keys that can be used for validating token signatures
+*/
+type JSONWebKeySet struct {
+	// Keys - All Keys available for signing under the set
+	Keys []JSONWebKey `json:"keys" bson:"keys"`
+}
+
+/*
 GetJWKS - Fetches all JSON Web Keys stored in the database and returns them as a slice. Only RSA Keys are returned with
 this function call, as this is intended to be used with the .well-known/jwks.json endpoint, and HSA secrets should not
 be exposed publicly as they are symmetrical
 
 TODO: Maybe rethink this to return only keys by a specific audience
 */
-func GetJWKS(serv *server.Server) (*jwkModel.JSONWebKeySet, error) {
-	jwks := new(jwkModel.JSONWebKeySet)
+func GetJWKS(serv *server.Server) (*JSONWebKeySet, error) {
+	jwks := new(JSONWebKeySet)
 
 	/*
 		This function call is actually fairly simple, as all we really need to do here is list out the entire collection.
