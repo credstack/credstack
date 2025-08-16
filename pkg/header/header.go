f1d95dd409157f5b1d742b592bd2286d6ddc6a31
Steven A. Zaluk <stevezaluk@gmail.com>
2025-08-15 20:49:32 -0400

    refactor: Updated NewHeader to return the newly created Go-native header models. Additionally renamed the function to New

--- pkg/header/header.go
+++ pkg/header/header.go
@@ -2,7 +2,6 @@ package header
 
 import (
 	internalTime "github.com/credstack/credstack/internal/time"
-	"github.com/credstack/credstack/pkg/models/header"
 	"github.com/credstack/credstack/pkg/secret"
 )
 
@@ -16,31 +15,27 @@ type Header struct {
 	Identifier string `json:"identifier" bson:"identifier"`
 
 	// CreatedAt - A unix timestamp representing when the object was created
-	CreatedAt int `json:"created_at" bson:"created_at"`
+	CreatedAt int64 `json:"created_at" bson:"created_at"`
 
 	// UpdatedAt - A unix timestamp representing when the object was last updated
-	UpdatedAt int `json:"updated_at" bson:"updated_at"`
+	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
 
 	// AccessedAt - A unix timestamp representing when the object was last accessed
-	AccessedAt int `json:"accessed_at" bson:"accessed_at"`
+	AccessedAt int64 `json:"accessed_at" bson:"accessed_at"`
 
 	// Tags - An arbitrary map of tags that can be assigned by the user
 	Tags map[string]string `json:"tags" bson:"tags"`
 }
 
 /*
-NewHeader - Generates a new header that can be attached to any cred-stack object. The basis that is provided in the
+New - Generates a new header that can be attached to any cred-stack object. The basis that is provided in the
 parameter of the function, is used for generating a version 5 UUID. Ideally, this should be a unique, immutable value
 to protect against de-duplication.
 */
-func NewHeader(basis string) *header.Header {
-	/*
-		Normally, I would inline this function call into each of the fields of the header.Header struct
-		however doing that could present slight discrepancies in each timestamp
-	*/
+func New(basis string) *Header {
 	timestamp := internalTime.UnixTimestamp()
 
-	return &header.Header{
+	return &Header{
 		Identifier: secret.GenerateUUID(basis),
 		CreatedAt:  timestamp,
 		UpdatedAt:  timestamp,
