65f0ec0df79eb0a1006148a138f6333d464b899e
Steven A. Zaluk <stevezaluk@gmail.com>
2025-08-15 20:47:59 -0400

    refactor: Implemented a go-native structure representing a header

--- pkg/header/header.go
+++ pkg/header/header.go
@@ -7,6 +7,28 @@ import (
 )
 
 /*
+Header - A message representing shared data that is applied to all objects created by credstack. Primarily holds a
+unique identifier that gets assigned to all user/system created objects, although also holds metadata such as timestamps
+that can be shared across many different types of objects
+*/
+type Header struct {
+	// Identifier - A UUID v5 based on an immutable property of the object this header is attached to.
+	Identifier string `json:"identifier" bson:"identifier"`
+
+	// CreatedAt - A unix timestamp representing when the object was created
+	CreatedAt int `json:"created_at" bson:"created_at"`
+
+	// UpdatedAt - A unix timestamp representing when the object was last updated
+	UpdatedAt int `json:"updated_at" bson:"updated_at"`
+
+	// AccessedAt - A unix timestamp representing when the object was last accessed
+	AccessedAt int `json:"accessed_at" bson:"accessed_at"`
+
+	// Tags - An arbitrary map of tags that can be assigned by the user
+	Tags map[string]string `json:"tags" bson:"tags"`
+}
+
+/*
 NewHeader - Generates a new header that can be attached to any cred-stack object. The basis that is provided in the
 parameter of the function, is used for generating a version 5 UUID. Ideally, this should be a unique, immutable value
 to protect against de-duplication.
