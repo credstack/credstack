c4aa9ea1c2028bec6511bf51b03027bd5ca4d673
Steven A. Zaluk <stevezaluk@gmail.com>
2025-11-27 18:33:29 -0500

    refactor: Moved RegisterHandler to UserService and re-named it PostUserHandler

--- internal/handlers/management/user.go
+++ internal/handlers/management/user.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/credstack/credstack/internal/middleware"
 	"github.com/credstack/credstack/internal/server"
+	"github.com/credstack/credstack/pkg/models/request"
+	"github.com/credstack/credstack/pkg/options"
 	"github.com/credstack/credstack/pkg/user"
 	"github.com/gofiber/fiber/v3"
 )
@@ -22,6 +24,7 @@ RegisterHandlers - Registers required handlers with the associated Fiber router
 */
 func (svc *UserService) RegisterHandlers() {
 	svc.group.Get("", svc.GetUserHandler)
+	svc.group.Post("", svc.PostUserHandler)
 	svc.group.Patch("", svc.PatchUserHandler)
 	svc.group.Delete("", svc.DeleteUserHandler)
 }
@@ -54,6 +57,35 @@ func (svc *UserService) GetUserHandler(c fiber.Ctx) error {
 	}
 
 	return c.JSON(requestedUser)
+}
+
+/*
+PostUserHandler - Provides a fiber handler for processing a POST request to /auth/register This should
+not be called directly, and should only ever be passed to fiber
+
+TODO: Authentication handler needs to happen here
+*/
+func (svc *UserService) PostUserHandler(c fiber.Ctx) error {
+	var registerRequest request.UserRegisterRequest
+
+	err := middleware.BindJSON(c, &registerRequest)
+	if err != nil {
+		return err
+	}
+
+	err = user.Register(
+		svc.server,
+		options.Credential().FromConfig(),
+		registerRequest.Email,
+		registerRequest.Username,
+		registerRequest.Password,
+	)
+
+	if err != nil {
+		return middleware.HandleError(c, err)
+	}
+
+	return c.Status(200).JSON(&fiber.Map{"message": "User successfully registered"}) // this should get its own response
 }
 
 /*
