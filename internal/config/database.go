16ae49011d17053774ac69d57e177b1a6265f4e2
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 16:59:00 -0500

    fix: Remigrated over methods for converting config values to mongo options, along with returning the default collections and index map

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
 
 type DatabaseConfig struct {
 	// Hostname - Defines the hostname that the MongoDB server can be accessed at
@@ -26,4 +32,76 @@ type DatabaseConfig struct {
 
 	// ConnectionTimeout - The duration that credstack should wait for before force closing a Mongo connection
 	ConnectionTimeout time.Duration `mapstructure:"connection_timeout"`
+}
+
+/*
+DefaultCollections - Returns the default collections that credstack expects to be able to read/write to. This
+is primarily used with Database.Init. This really shouldn't be changed so there is no setter defined for these
+*/
+func (config *DatabaseConfig) DefaultCollections() []string {
+	return []string{
+		"user",
+		"role",
+		"scope",
+		"client",
+		"resource_server",
+		"token",
+		"key",
+		"jwk",
+	}
+}
+
+/*
+IndexingMap - Returns the map used for creating indexes on the credstack's default collections. All the
+indexes listed here are created as unique indexes. This really shouldn't be changed so there is no setter
+defined for these
+*/
+func (config *DatabaseConfig) IndexingMap() map[string]bson.D {
+	return map[string]bson.D{
+		"user":            {{Key: "email", Value: 1}, {Key: "header.identifier", Value: 1}},
+		"role":            {{Key: "header.identifier", Value: 1}},
+		"scope":           {{Key: "header.identifier", Value: 1}},
+		"client":          {{Key: "client_id", Value: 1}, {Key: "header.identifier", Value: 1}},
+		"resource_server": {{Key: "header.identifier", Value: 1}},
+		"token":           {{Key: "token", Value: 1}},
+		"key":             {{Key: "header.identifier", Value: 1}},
+		"jwk":             {{Key: "kid", Value: 1}},
+	}
+}
+
+/*
+ToMongoOptions - Converts any pre-defined options declared in DatabaseConfig to an
+options.ClientOptions struct so that this can be used cleanly with the Database
+structure
+*/
+func (config *DatabaseConfig) ToMongoOptions() *options.ClientOptions {
+	/*
+		So realistically, SetDirect should probably be set to false here and
+		the DatabaseConfig structure should be modified so that multiple hosts
+		in a cluster can be used. I really don't think many people are going to
+		use this functionality to begin with so we will cross that bridge when
+		we come to it.
+	*/
+	clientOptions := options.Client().
+		SetHosts([]string{fmt.Sprintf("%s:%d", config.Hostname, config.Port)}).
+		SetDirect(true).
+		SetTimeout(config.ConnectionTimeout)
+
+	/*
+		Only SCRAM-SHA-256 is going to be set here as it provides a nice balance between
+		performance and security. This value isn't externalized either to the broader
+		DatabaseConfig structure so this shouldn't need to change
+	*/
+	const AuthMechanism = "SCRAM-SHA-256"
+
+	if config.UseAuthentication {
+		clientOptions.SetAuth(options.Credential{
+			AuthMechanism: AuthMechanism,
+			AuthSource:    config.AuthenticationDatabase,
+			Username:      config.Username,
+			Password:      config.Password,
+		})
+	}
+
+	return clientOptions
 }
