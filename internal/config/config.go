2b66a77a4767b638eb56a72600b3477778cd0f5e
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 16:09:20 -0500

    feature: Implemented a basic constructor for the config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
+import "github.com/spf13/viper"
+
 // Config Global configuration values for the entire application
 type Config struct {
+	viper *viper.Viper
+
 	// DatabaseConfig All database configuration options
 	DatabaseConfig *DatabaseConfig
 
@@ -10,4 +14,13 @@ type Config struct {
 	LogConfig *LogConfig
 
 	CredentialConfig *CredentialConfig
+}
+
+// New Initializes a new config structure along with a viper instance for values to be stored under
+func New() *Config {
+	config := &Config{
+		viper: viper.New(),
+	}
+
+	return config
 }
