184a9456b8a120c9fff6331749c32679974a12e0
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 17:15:40 -0500

    refactor: Re-migrated over methods for constructing FiberConfigs and ListenerConfigs

--- internal/config/api.go
+++ internal/config/api.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+)
+
 type ApiConfig struct {
 	// Port - The port number that the API should listen for requests on
 	Port int `mapstructure:"port"`
@@ -12,4 +18,42 @@ type ApiConfig struct {
 
 	// SkipPreflight - If set to true, then preflight checks are not conducted on API start
 	SkipPreflight bool `mapstructure:"skip_preflight"`
+}
+
+/*
+FiberConfig - Returns a fiber.Config  structure for the Api structure to consume
+*/
+func (config *ApiConfig) FiberConfig() fiber.Config {
+	fiberConfig := fiber.Config{
+		CaseSensitive:    true,
+		StrictRouting:    true,
+		DisableKeepalive: true,
+	}
+
+	if config.Debug {
+		fiberConfig.CaseSensitive = false
+		fiberConfig.StrictRouting = false
+		fiberConfig.IdleTimeout = 10 * time.Minute
+		fiberConfig.TrustProxy = true
+	}
+
+	return fiberConfig
+}
+
+/*
+ListenerConfig - Returns a fiber.ListenConfig structure for the Api structure to consume
+*/
+func (config *ApiConfig) ListenerConfig() fiber.ListenConfig {
+	listenConfig := fiber.ListenConfig{
+		DisableStartupMessage: true,
+		EnablePrefork:         config.Prefork,
+		ListenerNetwork:       fiber.NetworkTCP4,
+	}
+
+	if config.Debug {
+		listenConfig.EnablePrefork = false
+		listenConfig.EnablePrintRoutes = true
+	}
+
+	return listenConfig
 }
