30af2eb0306cc56744ccceb26d33d269cf0a1e60
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 16:21:11 -0500

    feature: Implemented support for Loading the config file from a default path

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
+	"path"
+	"strings"
+
+	"github.com/spf13/viper"
+)
 
 // Config Global configuration values for the entire application
 type Config struct {
@@ -18,6 +24,31 @@ type Config struct {
 
 	// CredentialConfig All user credential configuration options
 	CredentialConfig *CredentialConfig
+}
+
+// Load Loads the config from the requested file path and falls back to environmental variables
+// if the file was not found
+func (config *Config) Load(configPath string) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	sanitizedPath := strings.Replace(configPath, "~", home, 1)
+
+	config.viper.AddConfigPath(path.Dir(sanitizedPath))
+	config.viper.SetConfigType("json")
+	config.viper.SetConfigName("config.json")
+
+	err = config.viper.ReadInConfig()
+	if err != nil {
+		// Config file was not found, so lets fall back to environmental variables
+		viper.SetEnvPrefix("CREDSTACK")
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+		viper.AutomaticEnv()
+	}
+
+	return nil
 }
 
 // New Initializes a new config structure along with a viper instance for values to be stored under
