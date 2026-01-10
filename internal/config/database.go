783d103bccd48cc784acfa35c73eb75085aa0959
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 16:05:07 -0500

    feature: Migrated database configuration from the options package to config

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,3 +1,29 @@
 package config
 
+import "time"
 
+type DatabaseConfig struct {
+	// Hostname - Defines the hostname that the MongoDB server can be accessed at
+	Hostname string
+
+	// Port - Defines the port number that the MongoDB server is listening for connections on
+	Port uint32
+
+	// DefaultDatabase - Defines the default database that should be used for storing collections
+	DefaultDatabase string
+
+	// UseAuthentication - If set to false, then any other auth related configs wont be evaluated
+	UseAuthentication bool
+
+	// AuthenticationDatabase - Defines the database that should be used for authentication
+	AuthenticationDatabase string
+
+	// Username - Defines the username that should be used for authentication with MongoDB
+	Username string
+
+	// Password - Defines the password that should be used for authentication with MongoDB
+	Password string
+
+	// ConnectionTimeout - The duration that credstack should wait for before force closing a Mongo connection
+	ConnectionTimeout time.Duration
+}
