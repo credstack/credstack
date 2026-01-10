8f94dca4d489b38502546c55844d6693a54afbfa
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-10 16:11:07 -0500

    docs: Add comments for all member variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,15 +4,19 @@ import "github.com/spf13/viper"
 
 // Config Global configuration values for the entire application
 type Config struct {
+	// viper The viper instance that all configuration values will be stored under
 	viper *viper.Viper
 
 	// DatabaseConfig All database configuration options
 	DatabaseConfig *DatabaseConfig
 
+	// ApiConfig All API configuration options
 	ApiConfig *ApiConfig
 
+	// LogConfig All logging configuration options
 	LogConfig *LogConfig
 
+	// CredentialConfig All user credential configuration options
 	CredentialConfig *CredentialConfig
 }
 
