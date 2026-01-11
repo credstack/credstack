7e9690a067946a02785709e3d5876d8b21f4a880
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-11 15:14:15 -0500

    feature: Updated the ClientConfig structure with an option for timeout

--- internal/config/client.go
+++ internal/config/client.go
@@ -1,14 +1,20 @@
 package config
 
+import "time"
+
 // ClientConfig Represents all client configuration options. Primarily used in the API client or the CLI
 type ClientConfig struct {
 	// Url The Protocol + FQDN + Port to use for API calls made to credstack
 	Url string `mapstructure:"url"`
+
+	// Timeout The max amount of time in seconds to wait for a response from the API Server
+	Timeout time.Duration `mapstructure:"timeout"`
 }
 
 // DefaultClientConfig Initializes the ClientConfig structure with sane defaults
 func DefaultClientConfig() ClientConfig {
 	return ClientConfig{
-		Url: "http://localhost:8080",
+		Url:     "http://localhost:8080",
+		Timeout: 10 * time.Second,
 	}
 }
