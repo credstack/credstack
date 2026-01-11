df81f3450622e4a1e0df03a360714e3a366988b7
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-11 15:05:39 -0500

    feature: Implemented boilerplate for the Api client

--- pkg/apiclient/apiclient.go
+++ pkg/apiclient/apiclient.go
@@ -1,3 +1,15 @@
 package apiclient
 
+import "github.com/credstack/credstack/internal/config"
 
+type ApiClient struct {
+	// config The ClientConfig for the API client
+	config config.ClientConfig
+}
+
+// New Initialize a new credstack API client
+func New(config config.ClientConfig) *ApiClient {
+	return &ApiClient{
+		config: config,
+	}
+}
