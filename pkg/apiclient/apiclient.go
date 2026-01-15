976d23d0d5b90d587c1817f71bc088c540ecb1ea
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-14 19:19:23 -0500

    refactor: Migrated http.Client initialization code into HTTPResource structure

--- pkg/apiclient/apiclient.go
+++ pkg/apiclient/apiclient.go
@@ -1,32 +1,17 @@
 package apiclient
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/credstack/credstack/internal/config"
 )
 
 type ApiClient struct {
 	// config The ClientConfig for the API client
 	config config.ClientConfig
-
-	// client The http.Client that gets used for all requests
-	client *http.Client
 }
 
 // New Initialize a new credstack API client
 func New(config config.ClientConfig) *ApiClient {
-	client := &http.Client{
-		Transport: &http.Transport{
-			DisableCompression: true,
-			IdleConnTimeout:    30 * time.Second,
-			MaxIdleConns:       5,
-		},
-	}
-
 	return &ApiClient{
 		config: config,
-		client: client,
 	}
 }
--- pkg/apiclient/internal/client.go
+++ pkg/apiclient/internal/client.go
@@ -1,3 +1,26 @@
 package internal
 
+import (
+	"net/http"
+	"time"
+)
 
+// HTTPResource Overarching abstraction that provides common functionality for all HTTP resources
+type HTTPResource struct {
+	// client The http.Client that gets used for all requests
+	client *http.Client
+}
+
+func New() *HTTPResource {
+	client := &http.Client{
+		Transport: &http.Transport{
+			DisableCompression: true,
+			IdleConnTimeout:    30 * time.Second,
+			MaxIdleConns:       5,
+		},
+	}
+
+	return &HTTPResource{
+		client: client,
+	}
+}
