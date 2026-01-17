5fce164f932614497bfccc55436e14609fc49fac
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-17 17:23:05 -0500

    feature: Implemented a basic function for building requests

--- pkg/apiclient/internal/client.go
+++ pkg/apiclient/internal/client.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"net/http"
+	"runtime"
 	"time"
 )
 
@@ -9,6 +10,20 @@ import (
 type HTTPResource struct {
 	// client The http.Client that gets used for all requests
 	client *http.Client
+}
+
+// BuildRequest Builds a request based with headers inserted for identification and authorization
+func (resource *HTTPResource) BuildRequest(method string, uri string) (*http.Request, error) {
+	req, err := http.NewRequest(method, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "credstack-api-client/1.0 ("+runtime.GOOS+") ("+runtime.GOARCH+")")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	return req, nil
 }
 
 func New() *HTTPResource {
