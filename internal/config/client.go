2c6320141e5566f03b7628c22e79de629fda02a5
Steven A. Zaluk <stevezaluk@gmail.com>
2026-01-17 16:40:25 -0500

    feature: Updated ClientConfig to support backoff's and retrying requests

--- internal/config/client.go
+++ internal/config/client.go
@@ -9,12 +9,20 @@ type ClientConfig struct {
 
 	// Timeout The max amount of time in seconds to wait for a response from the API Server
 	Timeout time.Duration `mapstructure:"timeout"`
+
+	// Retry The amount of times to retry a request should an error occur
+	Retry int `mapstructure:"retry"`
+
+	// BackoffDuration The amount of time to wait before retrying a request
+	BackoffDuration time.Duration `mapstructure:"backoff"`
 }
 
 // DefaultClientConfig Initializes the ClientConfig structure with sane defaults
 func DefaultClientConfig() ClientConfig {
 	return ClientConfig{
-		Url:     "http://localhost:8080",
-		Timeout: 10 * time.Second,
+		Url:             "http://localhost:8080",
+		Timeout:         10 * time.Second,
+		Retry:           3,
+		BackoffDuration: 10 * time.Millisecond,
 	}
 }
