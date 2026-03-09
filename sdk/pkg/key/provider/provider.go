da2c417ac6c12ea7a7b66999951030c2d4797ba7
Steven A. Zaluk <stevezaluk@gmail.com>
2026-03-09 11:10:30 -0400

    feat: Updated provider interface to expose a Count function which will determine how many keys are available for signing

--- sdk/pkg/key/provider/provider.go
+++ sdk/pkg/key/provider/provider.go
@@ -4,6 +4,9 @@ import "github.com/credstack/credstack/sdk/pkg/key"
 
 // Provider Represents a Key Provider that can be used to fetch keys for signing
 type Provider interface {
+	// Count Returns the current number of keys that are available for signing
+	Count(bool) (int, error)
+
 	// ActiveKey Returns the currently active signing key for JWT's
 	ActiveKey(string, string) (key.PrivateKey, error)
 
